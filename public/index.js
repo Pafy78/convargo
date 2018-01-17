e989108941082db06b138dcc671e922e3c93dbc6
Pafy78 <pierre.baudoin@devinci.fr>
2018-01-17 14:48:08 +0100

    Code re-organisation

--- public/index.js
+++ public/index.js
@@ -3,20 +3,20 @@
 //list of truckers
 //useful for ALL 5 exercises
 var truckers = [{
-  'id': 'f944a3ff-591b-4d5b-9b67-c7e08cba9791',
-  'name': 'les-routiers-bretons',
-  'pricePerKm': 0.05,
-  'pricePerVolume': 5
+    'id': 'f944a3ff-591b-4d5b-9b67-c7e08cba9791',
+    'name': 'les-routiers-bretons',
+    'pricePerKm': 0.05,
+    'pricePerVolume': 5
 }, {
-  'id': '165d65ec-5e3f-488e-b371-d56ee100aa58',
-  'name': 'geodis',
-  'pricePerKm': 0.1,
-  'pricePerVolume': 8.5
+    'id': '165d65ec-5e3f-488e-b371-d56ee100aa58',
+    'name': 'geodis',
+    'pricePerKm': 0.1,
+    'pricePerVolume': 8.5
 }, {
-  'id': '6e06c9c0-4ab0-4d66-8325-c5fa60187cf8',
-  'name': 'xpo',
-  'pricePerKm': 0.10,
-  'pricePerVolume': 10
+    'id': '6e06c9c0-4ab0-4d66-8325-c5fa60187cf8',
+    'name': 'xpo',
+    'pricePerKm': 0.10,
+    'pricePerVolume': 10
 }];
 
 //list of current shippings
@@ -25,155 +25,158 @@ var truckers = [{
 //The `commission` is updated from exercice 3
 //The `options` is useful from exercice 4
 var deliveries = [{
-  'id': 'bba9500c-fd9e-453f-abf1-4cd8f52af377',
-  'shipper': 'bio-gourmet',
-  'truckerId': 'f944a3ff-591b-4d5b-9b67-c7e08cba9791',
-  'distance': 100,
-  'volume': 4,
-  'options': {
-    'deductibleReduction': false
-  },
-  'price': 0,
-  'commission': {
-    'insurance': 0,
-    'treasury': 0,
-    'convargo': 0
-  }
+    'id': 'bba9500c-fd9e-453f-abf1-4cd8f52af377',
+    'shipper': 'bio-gourmet',
+    'truckerId': 'f944a3ff-591b-4d5b-9b67-c7e08cba9791',
+    'distance': 100,
+    'volume': 4,
+    'options': {
+        'deductibleReduction': false
+    },
+    'price': 0,
+    'commission': {
+        'insurance': 0,
+        'treasury': 0,
+        'convargo': 0
+    }
 }, {
-  'id': '65203b0a-a864-4dea-81e2-e389515752a8',
-  'shipper': 'librairie-lu-cie',
-  'truckerId': '165d65ec-5e3f-488e-b371-d56ee100aa58',
-  'distance': 650,
-  'volume': 12,
-  'options': {
-    'deductibleReduction': true
-  },
-  'price': 0,
-  'commission': {
-    'insurance': 0,
-    'treasury': 0,
-    'convargo': 0
-  }
+    'id': '65203b0a-a864-4dea-81e2-e389515752a8',
+    'shipper': 'librairie-lu-cie',
+    'truckerId': '165d65ec-5e3f-488e-b371-d56ee100aa58',
+    'distance': 650,
+    'volume': 12,
+    'options': {
+        'deductibleReduction': true
+    },
+    'price': 0,
+    'commission': {
+        'insurance': 0,
+        'treasury': 0,
+        'convargo': 0
+    }
 }, {
-  'id': '94dab739-bd93-44c0-9be1-52dd07baa9f6',
-  'shipper': 'otacos',
-  'truckerId': '6e06c9c0-4ab0-4d66-8325-c5fa60187cf8',
-  'distance': 1250,
-  'volume': 30,
-  'options': {
-    'deductibleReduction': true
-  },
-  'price': 0,
-  'commission': {
-    'insurance': 0,
-    'treasury': 0,
-    'convargo': 0
-  }
+    'id': '94dab739-bd93-44c0-9be1-52dd07baa9f6',
+    'shipper': 'otacos',
+    'truckerId': '6e06c9c0-4ab0-4d66-8325-c5fa60187cf8',
+    'distance': 1250,
+    'volume': 30,
+    'options': {
+        'deductibleReduction': true
+    },
+    'price': 0,
+    'commission': {
+        'insurance': 0,
+        'treasury': 0,
+        'convargo': 0
+    }
 }];
 
 //list of actors for payment
 //useful from exercise 5
 const actors = [{
-  'deliveryId': 'bba9500c-fd9e-453f-abf1-4cd8f52af377',
-  'payment': [{
-    'who': 'shipper',
-    'type': 'debit',
-    'amount': 0
-  }, {
-    'who': 'trucker',
-    'type': 'credit',
-    'amount': 0
-  }, {
-    'who': 'insurance',
-    'type': 'credit',
-    'amount': 0
-  }, {
-    'who': 'treasury',
-    'type': 'credit',
-    'amount': 0
-  }, {
-    'who': 'convargo',
-    'type': 'credit',
-    'amount': 0
-  }]
+    'deliveryId': 'bba9500c-fd9e-453f-abf1-4cd8f52af377',
+    'payment': [{
+        'who': 'shipper',
+        'type': 'debit',
+        'amount': 0
+    }, {
+        'who': 'trucker',
+        'type': 'credit',
+        'amount': 0
+    }, {
+        'who': 'insurance',
+        'type': 'credit',
+        'amount': 0
+    }, {
+        'who': 'treasury',
+        'type': 'credit',
+        'amount': 0
+    }, {
+        'who': 'convargo',
+        'type': 'credit',
+        'amount': 0
+    }]
 }, {
-  'deliveryId': '65203b0a-a864-4dea-81e2-e389515752a8',
-  'payment': [{
-    'who': 'shipper',
-    'type': 'debit',
-    'amount': 0
-  }, {
-    'who': 'trucker',
-    'type': 'credit',
-    'amount': 0
-  }, {
-    'who': 'insurance',
-    'type': 'credit',
-    'amount': 0
-  }, {
-    'who': 'treasury',
-    'type': 'credit',
-    'amount': 0
-  }, {
-    'who': 'convargo',
-    'type': 'credit',
-    'amount': 0
-  }]
+    'deliveryId': '65203b0a-a864-4dea-81e2-e389515752a8',
+    'payment': [{
+        'who': 'shipper',
+        'type': 'debit',
+        'amount': 0
+    }, {
+        'who': 'trucker',
+        'type': 'credit',
+        'amount': 0
+    }, {
+        'who': 'insurance',
+        'type': 'credit',
+        'amount': 0
+    }, {
+        'who': 'treasury',
+        'type': 'credit',
+        'amount': 0
+    }, {
+        'who': 'convargo',
+        'type': 'credit',
+        'amount': 0
+    }]
 }, {
-  'deliveryId': '94dab739-bd93-44c0-9be1-52dd07baa9f6',
-  'payment': [{
-    'who': 'shipper',
-    'type': 'debit',
-    'amount': 0
-  }, {
-    'who': 'trucker',
-    'type': 'credit',
-    'amount': 0
-  }, {
-    'who': 'treasury',
-    'type': 'credit',
-    'amount': 0
-  }, {
-    'who': 'insurance',
-    'type': 'credit',
-    'amount': 0
-  }, {
-    'who': 'convargo',
-    'type': 'credit',
-    'amount': 0
-  }]
+    'deliveryId': '94dab739-bd93-44c0-9be1-52dd07baa9f6',
+    'payment': [{
+        'who': 'shipper',
+        'type': 'debit',
+        'amount': 0
+    }, {
+        'who': 'trucker',
+        'type': 'credit',
+        'amount': 0
+    }, {
+        'who': 'treasury',
+        'type': 'credit',
+        'amount': 0
+    }, {
+        'who': 'insurance',
+        'type': 'credit',
+        'amount': 0
+    }, {
+        'who': 'convargo',
+        'type': 'credit',
+        'amount': 0
+    }]
 }];
 
 
 
-
-function Step1(){
-    var distance, volume, trucker;
+function ComputePrice(){
     deliveries.forEach(function(deliverie){
-        trucker = truckers.find(function(element){
-            return element.id = deliverie.truckerId;
-        });
-        distance = trucker.pricePerKm * deliverie.distance;
-        volume = trucker.pricePerVolume * deliverie.volume;
-        deliverie.price = distance + volume;
+        deliverie.price = GetPriceBase(deliverie);
+        deliverie.price = GetPriceRemise(deliverie);
     });
 }
-function Step2(){
-    deliveries.forEach(function(deliverie){
-        if(deliverie.volume > 25){
-            deliverie.price -= deliverie.price * 0.5;
-        }
-        else if(deliverie.volume > 10){
-            deliverie.price -= deliverie.price * 0.3;
-        }
-        else if(deliverie.volume > 5){
-            deliverie.price -= deliverie.price * 0.1;
-        }
-    }); 
+function GetPriceBase(deliverie){
+    var distance, volume, trucker;
+    trucker = truckers.find(function(element){
+        return element.id = deliverie.truckerId;
+    });
+    distance = trucker.pricePerKm * deliverie.distance;
+    volume = trucker.pricePerVolume * deliverie.volume;
+    return distance + volume;
+}
+function GetPriceRemise(deliverie){
+    if(deliverie.volume > 25){
+        return deliverie.price * 0.5;
+    }
+    else if(deliverie.volume > 10){
+        return deliverie.price * 0.3;
+    }
+    else if(deliverie.volume > 5){
+        return deliverie.price * 0.1;
+    } 
+    return deliverie.price;
 }
 
-Step1();
-Step2();
+
+
+ComputePrice();
 
 
 console.log(truckers);
